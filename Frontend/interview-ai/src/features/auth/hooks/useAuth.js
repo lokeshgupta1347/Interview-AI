ae3b5c4a6875ead8ebd626b4873920ac8f7008e9
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-06-02 22:33:50 +0530

    authentication part done -register,login

--- Frontend/interview-ai/src/features/auth/hooks/useAuth.js
+++ Frontend/interview-ai/src/features/auth/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext,useEffect } from "react";
 import { AuthContext } from "../auth.context";
 import { login,register,logout,getMe } from "../services/auth.api";
 
@@ -51,6 +51,20 @@ const handleLogout = async () => {
         setLoading(false)
         }
     }
+
+    useEffect(()=>{
+        const getAndSetUser=async()=>{
+        const data=await getMe()
+        setUser(data.user)
+        setLoading(false)
+      }
+
+      getAndSetUser()
+    },[])
+
+
+
+
 
     return {user,loading,handleLogin,handleRegister,handleLogout}
 
